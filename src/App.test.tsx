4bbbed8c1a4862c62cce523b2bf00744a5b80d1f
Pavel Shermayster <sherm84@gmail.com>
2019-01-06 12:16:08 +0200

    feat: added react testing library example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { render, getByText, cleanup } from 'react-testing-library';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+test('getComputedStyle', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<App />, div);
-	const text = div.querySelector('span');
+	const text = div.querySelector('span')!;
 	const styles = getComputedStyle(text);
 	console.log(styles.color);
 	expect(styles.color).toBe('black');
 	ReactDOM.unmountComponentAtNode(div);
+});
+
+test('react-testing-library', () => {
+	const el = render(
+		<div id="root">
+			<App />
+		</div>
+	);
+	const text = el.getByText('Hi!');
+	const styles = getComputedStyle(text);
+	expect(styles.color).toBe('black');
 });
